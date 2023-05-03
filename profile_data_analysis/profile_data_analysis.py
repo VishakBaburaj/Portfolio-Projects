4320695b1a734e83d448b9151fd7c9b51a15fe00
Vishak Baburaj <vishakbaburaj98@gmail.com>
2023-05-03 23:22:52 +0530

    First commit

---
+++ profile_data_analysis/profile_data_analysis.py
@@ -0,0 +1,22 @@
+# # Importing necessary libraries
+# import streamlit as st
+# import pandas as pd
+
+# st.set_option('deprecation.showfileUploaderEncoding', False)  # Disable warning message
+
+# st.title('LinkedIn Data Analysis')
+
+# uploaded_file = st.file_uploader('Choose a CSV file', type = 'csv')
+
+# if uploaded_file is not None:
+#     # Read the CSV file
+#     data = pd.read_csv(uploaded_file)
+
+#     # Process the data
+#     processed_data = data
+
+#     # Display the processed data
+
+
+
+# processed_data = pd.read_csv('')
