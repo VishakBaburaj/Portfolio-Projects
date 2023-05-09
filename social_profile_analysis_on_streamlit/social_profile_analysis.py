580353e9d469ebaef8fc1508bb98d1dd34debdfe
Vishak Baburaj <vishakbaburaj98@gmail.com>
2023-05-10 03:37:47 +0530

    Created LinkedIn Monthly Trend Viz

--- social_profile_analysis_on_streamlit/social_profile_analysis.py
+++ social_profile_analysis_on_streamlit/social_profile_analysis.py
@@ -2,10 +2,11 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import datetime as dt
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Setting the page title and layout
-st.set_page_config(page_title = 'LinkedIn Data Analysis',
+st.set_page_config(page_title = 'LinkedIn Job Application Tracker',
                    page_icon=None,
                    layout = 'centered', initial_sidebar_state="auto")
 
@@ -31,85 +32,110 @@ def process_linkedin_job_app_data(data):
     data['Date'] = pd.to_datetime(data['Date'])
     # Droping the index column from the DataFrame
     data = data.reset_index(drop=True)
+    # Extracting weekday, month and year into separate columns
+    data['Weekday'] = data['Date'].dt.day_name()
+    data['Month'] = data['Date'].dt.strftime('%B')
+    data['Year'] = data['Date'].dt.year
     return data
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-# About page
-about_page = st.sidebar.button('About the app')
+# Setting the sidebar options
+options = ['About','LinkedIn']
+selected_option = st.sidebar.selectbox('Select an option', options)
 
-# Adding a horizontal line between the about and first level options
-st.sidebar.markdown('---')
-
-# Setting the first level sidebar options
-options_level_1 = ['LinkedIn','Instagram']
-selected_option_level_1 = st.sidebar.selectbox('Choose an application', options_level_1)
-
-# Adding a horizontal line between the first and second level options
+# Adding a horizontal line undernead the sidebar
 st.sidebar.markdown('---')
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-# If the selected option is "About the app", display the 'About the app' page
-if about_page:
-    st.markdown('## About')
+# If the selected option is "About", display the 'About the app' page
+if selected_option == 'About':
+    st.markdown('## About the app')
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-# If the selected option is "LinkedIn", display the second level of the sidebar
-elif selected_option_level_1 == 'LinkedIn':
-    options_level_2_for_linkedin = ['LinkedIn Job Application', 'LinkedIn Connections']
-    selected_option_level_2_for_linkedin = st.sidebar.radio('Choose an insight', options_level_2_for_linkedin)
+# If the selected option is "LinkedIn", display the "LinkedIn Job Application Tracker" page
+elif selected_option == 'LinkedIn':
 
-    # Setting sidebar "LinkedIn" - "LinkedIn Job Application"
-    if selected_option_level_2_for_linkedin  == 'LinkedIn Job Application':
+    # Setting title of the page
+    st.markdown('## LinkedIn Job Application Tracker')
 
-        # Setting title of the page
-        st.markdown('## LinkedIn Job Application')
+    # Disabling the warning message
+    st.set_option('deprecation.showfileUploaderEncoding', False)
 
-        st.set_option('deprecation.showfileUploaderEncoding', False)  # Disable warning message
+    # Checkbox to load sample data
+    load_sample_data_checkbox = st.checkbox('Load Sample Data')
 
-        # Checkbox to load sample data
-        load_sample_data_checkbox = st.checkbox('Load Sample Data')
+    # Reading sample csv data
+    df = pd.read_csv("C:/Users/visha/Documents/Projects/Portfolio Projects/social_profile_analysis_on_streamlit/sample_data/Job Applications.csv") 
 
-        # Reading sample csv data
-        df = pd.read_csv("C:/Users/visha/Documents/Projects/Portfolio Projects/social_profile_analysis_on_streamlit/sample_data/Job Applications.csv") 
+    # If the sample data checkbox is selected upload the sample data
+    if load_sample_data_checkbox:
+        uploaded_file = df  # Display the sample data in the app
 
-        # If the sample data checkbox is selected upload the sample data
-        if load_sample_data_checkbox:
-            uploaded_file = df  # Display the sample data in the app
+    # If the sample data checkbox is not selected upload the csv data
+    elif uploaded_file := st.file_uploader('Choose the Job Application CSV file', type = 'csv'):
 
-        # If the sample data checkbox is not selected upload the csv data
-        elif uploaded_file := st.file_uploader('Choose the Job Application CSV file', type = 'csv'):
+        # Reading the CSV file
+        uploaded_file = pd.read_csv(uploaded_file)
 
-            # Reading the CSV file
-            uploaded_file = pd.read_csv(uploaded_file)
+    # When either data has been input
+    if uploaded_file is not None:
 
-        # When either data is has been input
-        if uploaded_file is not None:
+        # Precessing the data
+        data = process_linkedin_job_app_data(uploaded_file)
 
-            # Precessing the data
-            data = process_linkedin_job_app_data(uploaded_file)
-
-            # Adding a horizontal line
-            st.write('---')
+        # Adding a horizontal line
+        st.write('---')
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-            # Total jobs applied
-            total_jobs_applied = len(data)
+        # Divide the screen into two equal parts
+        col1, col2 = st.columns(2)
 
-            # Displaying the total count of jobs applied as a KPI
-            if total_jobs_applied >= 0:
-                st.success(f"**Total Jobs Applied:** {total_jobs_applied}")
-            else:
-                st.error(f"**Total Jobs Applied:** {total_jobs_applied}")
+        # Total jobs applied KPI
+        total_jobs_applied = len(data)
 
-            # Adding a horizontal line
-            st.write('---')
+        # Displaying the total count of jobs applied as a KPI in the first column
+        if total_jobs_applied >= 0:
+            col1.success(f"**Total Jobs Applied:** {total_jobs_applied}")
+        else:
+            col1.error(f"**Total Jobs Applied:** {total_jobs_applied}")
+
+
+        # Average jobs applied per day KPI
+        #average_jobs_per_day = total_jobs_applied / 7 # 7 days in a week
+        
+        # Calculate the number of days over which the jobs were applied
+        num_days = (data['Date'].max() - data['Date'].min()).days + 1
+
+        # Calculate the total number of jobs applied
+        total_jobs_applied = len(data)
+
+        # Calculate the average jobs applied per day
+        average_jobs_per_day = total_jobs_applied / num_days
+
+        # Displaying the average jobs applied per day as a KPI in the second column
+        if average_jobs_per_day >= 0:
+            col2.success(f"**Average Jobs Applied Per Day:** {average_jobs_per_day:.0f}")
+        else:
+            col2.error(f"**Average Jobs Applied Per Day:** {average_jobs_per_day:.0f}")
+
+        # Adding a horizontal line
+        st.write('---')
+
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
+        # Selecting the job roles and companies
+        top_10_options = ['Top 10 Job Roles Applied', 'Top 10 Companies Applied']
+        selected_top_10_options = st.radio('###### Select an insight:', top_10_options)
+
+        # Displaying the selected options
+        if selected_top_10_options == 'Top 10 Job Roles Applied':
             # Grouping the data by job title and counting the number of occurrences
             jobs_applied = data.groupby('Job Title').size().reset_index(name='count')
 
@@ -117,17 +143,15 @@ elif selected_option_level_1 == 'LinkedIn':
             top_jobs_applied = jobs_applied.sort_values("count", ascending=False).head(10)
 
             # Creating the chart
-            chart = alt.Chart(top_jobs_applied).mark_bar().encode(
+            top_jobs_applied_chart = alt.Chart(top_jobs_applied).mark_bar().encode(
                 x=alt.X('count:Q', title='Count of Jobs Applied'),
                 y=alt.Y('Job Title:O', title='Job Titles', sort='-x'),
                 color=alt.Color('count:Q', legend=None),
                 tooltip=['Job Title:O', 'count:Q']
-            ).properties(
-                title='Top 10 Jobs Applied'
             )
 
             # Adding labels for the count of jobs applied
-            text = chart.mark_text(
+            top_jobs_applied_text = top_jobs_applied_chart.mark_text(
                 align='left',
                 baseline='middle',
                 dx=5,
@@ -136,14 +160,15 @@ elif selected_option_level_1 == 'LinkedIn':
                 text='count:Q'
             )
 
+            st.write(f"#### Top 10 Job Roles Applied")
+
             # Displaying the chart and the count of jobs applied
-            st.altair_chart(chart + text, use_container_width=True)
+            st.altair_chart(top_jobs_applied_chart + top_jobs_applied_text, use_container_width=True)
 
             # Adding a horizontal line
             st.write('---')
 
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
-
+        elif selected_top_10_options == 'Top 10 Companies Applied':
             # Grouping the data by job title and counting the number of occurrences
             companies_applied = data.groupby('Company Name').size().reset_index(name='count')
 
@@ -151,17 +176,15 @@ elif selected_option_level_1 == 'LinkedIn':
             top_companies_applied = companies_applied.sort_values("count", ascending=False).head(10)
 
             # Creating the chart
-            chart = alt.Chart(top_companies_applied).mark_bar().encode(
+            top_companies_applied_chart = alt.Chart(top_companies_applied).mark_bar().encode(
                 x=alt.X('count:Q', title='Count of Companies Applied'),
                 y=alt.Y('Company Name:O', title='Company Names', sort='-x'),
                 color=alt.Color('count:Q', legend=None),
                 tooltip=['Company Name:O', 'count:Q']
-            ).properties(
-                title='Top 10 Companies Applied'
             )
 
             # Adding labels for the count of jobs applied
-            text = chart.mark_text(
+            top_companies_applied_text = top_companies_applied_chart.mark_text(
                 align='left',
                 baseline='middle',
                 dx=5,
@@ -170,14 +193,22 @@ elif selected_option_level_1 == 'LinkedIn':
                 text='count:Q'
             )
 
+            st.write(f"#### Top 10 Companies Applied")
+
             # Displaying the chart and the count of jobs applied
-            st.altair_chart(chart + text, use_container_width=True)
+            st.altair_chart(top_companies_applied_chart + top_companies_applied_text, use_container_width=True)
 
             # Adding a horizontal line
             st.write('---')
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-            
+
+        # Selecting the trend
+        trend_options = ['Daily Application Trend', 'Weekly Applications Trend', 'Monthly Application Trend']
+        selected_trend = st.radio('###### Select a trend:', trend_options)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+        if selected_trend == 'Daily Application Trend':
             # Grouping the DataFrame by the date column and counting the number of applications for each day
             daily_applications = data.groupby('Date').size().reset_index(name='count')
 
@@ -212,37 +243,51 @@ elif selected_option_level_1 == 'LinkedIn':
                 ).add_selection(hover)
             )
 
-            daily_app_chart = (lines + points + tooltips).interactive().configure_view(strokeWidth=0)
+            daily_applications_chart = (lines + points + tooltips).interactive().configure_view(strokeWidth=0)
 
-            st.write(f"###### Daily Job Applications Trend")
-            st.altair_chart(daily_app_chart, use_container_width=True)
+            st.write(f"#### Daily Job Applications Trend")
+            st.altair_chart(daily_applications_chart, use_container_width=True)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-    # Setting sidebar "LinkedIn" - "LinkedIn Connections Insights"
-    elif selected_option_level_2_for_linkedin  == 'LinkedIn Connections':
+        elif selected_trend == 'Weekly Applications Trend':
+            st.write('You clicked Button 2!')
 
-        # Setting title of the page
-        st.markdown('## LinkedIn Connections')
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-        st.set_option('deprecation.showfileUploaderEncoding', False)  # Disable warning message
+        elif selected_trend == 'Monthly Application Trend':
+            # Group the data by year and month, and count the number of applications per month
+            monthly_applications = data.groupby(['Year', 'Month'])['Date'].count().reset_index()
+            monthly_applications = monthly_applications.rename(columns={'Date': 'Count'})
 
+            # Create a new column that combines the Year and Month columns
+            monthly_applications['YearMonth'] = pd.to_datetime(monthly_applications['Year'].astype(str) + ' ' + monthly_applications['Month'], format='%Y %B')
+            monthly_applications = monthly_applications.sort_values('YearMonth')
 
-# If the selected option is "Instagram", display the second level of the sidebar
-elif selected_option_level_1 == 'Instagram':
-    options_level_2_for_instagram = ['Post', 'Reel']
-    selected_option_level_2_for_instagram = st.sidebar.radio('Choose an insight', options_level_2_for_instagram)
+            st.write(f"#### Monthly Job Applications Trend")
 
-    # Setting sidebar "Instagram" - "Option 1"
-    if selected_option_level_2_for_instagram  == 'Post':
-        st.markdown('## Coming Soon')
+            # Creating a dropdown filters for year
+            monthly_applications_selected_year = st.selectbox('Select year:', options=monthly_applications['Year'].unique(), index=0)
 
+            # Creating the year filters
+            monthly_applications_year_filter = monthly_applications[monthly_applications['Year'] == monthly_applications_selected_year]
 
-    # Setting sidebar "Instagram" - "Option 2"
-    elif selected_option_level_2_for_instagram  == 'Reel':
-        st.markdown('## Coming Soon')
+            # Creating the bar chart
+            monthly_applications_chart = alt.Chart(monthly_applications_year_filter).mark_bar().encode(
+                x=alt.X('Month:N', title='Month', sort=None),
+                y=alt.Y('Count:Q', title='Count of Applications Sent'),
+                color=alt.Color('Count:Q', title='Year', legend=None),
+                tooltip=['Year:N', 'Month:N', 'Count:Q']
+            )
+                
+            # Add labels to the bars
+            monthly_applications_text = monthly_applications_chart.mark_text(
+                align='center',
+                baseline='bottom',
+                dy=-5,
+                fontSize=18
+            ).encode(
+                text='Count:Q'
+            )
 
-
-
-
-
-
+            # Displaying the chart
+            st.altair_chart((monthly_applications_chart + monthly_applications_text), use_container_width=True)
